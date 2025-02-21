50d0a07dd6f301e3abc2863ffdbc16469b26f1ba
saeed ayman <saidayman550@gmail.com>
2025-02-21 23:14:47 +0200

    add brand adapter - state - viewmodel - and brand fragment

--- app/src/main/java/com/example/test/presentation/ui/brands/BrandsFragment.kt
+++ app/src/main/java/com/example/test/presentation/ui/brands/BrandsFragment.kt
@@ -1,56 +1,97 @@
 package com.example.test.presentation.ui.brands
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.example.test.R
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.example.test.databinding.FragmentBrandsBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
-/**
- * A simple [Fragment] subclass.
- * Use the [BrandsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class BrandsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+@AndroidEntryPoint
+class BrandsFragment : Fragment(R.layout.fragment_brands) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private lateinit var binding: FragmentBrandsBinding
+    private val viewModel: BrandViewModel by viewModels()
+    private val brandAdapter = BrandAdapter()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentBrandsBinding.bind(view)
+
+        viewModel.getBrands()
+        handleBrandsState()
+        searchForBrand()
+
+
+    }
+
+    private fun handleBrandsState() {
+        lifecycleScope.launch {
+
+            viewModel.brandStateFlow
+                .flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
+                .collect { state ->
+                    when {
+                        state.loading -> {
+                            //handle loading
+                        }
+
+                        state.data.isNotEmpty() -> setUpAdapter(state)
+
+                        state.unKnownError.isNotEmpty() ->
+                            Toast.makeText(requireContext(), state.unKnownError, Toast.LENGTH_SHORT)
+                                .show()
+
+                        state.unauthorizedError ->
+                            Toast.makeText(
+                                requireContext(),
+                                "unauthorizedError",
+                                Toast.LENGTH_SHORT
+                            )
+                                .show()
+
+                        state.notFoundError ->
+                            Toast.makeText(
+                                requireContext(),
+                                "notFoundError",
+                                Toast.LENGTH_SHORT)
+                                .show()
+                    }
+                }
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_brands, container, false)
+    private fun setUpAdapter(state: BrandsState) {
+        viewModel.brandList = state.data.toMutableList()
+        brandAdapter.updateList(state.data)
+        binding.brandRec.adapter = brandAdapter
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BrandsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                BrandsFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
+    private fun searchForBrand() {
+        binding.brandEditSearch.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                val l = viewModel.brandList.filter { brandsData ->
+                    brandsData.name.contains(s.toString(), ignoreCase = true)
                 }
+                brandAdapter.updateList(l)
+            }
+        })
     }
+
+
 }
